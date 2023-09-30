e90a2ecccadbe03f5653ea5a8f762d73f5aca0a3
MadAlex1997 <139396447+MadAlex1997@users.noreply.github.com>
2023-09-30 17:55:41 -0400

    Update sim_configure.py
    Add more sleeping to prevent premature pulls from the serial port, and add ping test for usb0 which should be the network connection from the sim card

--- sim_configure.py
+++ sim_configure.py
@@ -7,24 +7,30 @@ sleep(5)
 if stream.inWaiting():
     rec_buff = stream.read(stream.inWaiting()).decode()
     print(rec_buff)
+    
     stream.write("at+cenablelan=1\r\n".encode())
+    sleep(5)
     if stream.inWaiting():
         rec_buff = stream.read(stream.inWaiting()).decode()
         print(rec_buff)
         stream.write("AT+CUSBPIDSWITCH=9011,1,1\r\n".encode())
+        sleep(30)
         if stream.inWaiting():
             rec_buff = stream.read(stream.inWaiting()).decode()
             print(rec_buff)
             stream.write("at+gpsauto=1\r\n".encode())
+            sleep(5)
             if stream.inWaiting():
                 rec_buff = stream.read(stream.inWaiting()).decode()
                 print(rec_buff)
 stream.close()
 sleep(5)
+import os
 for i in range(500):
     gps = get_gps()
     if gps["gps_valid"]:
         print("GPS functional")
         gps()
+        os.system("ping -I usb0 -w 5 8.8.8.8")
         break
 
